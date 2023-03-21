90c84f7ba2178561d7f9f914001c2e3ebac965d9
Omotoso Olatomiwa <tommyomotouch@gmail.com>
2023-03-21 13:31:45 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor (Math.random () * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random () * 6) + 1;
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
